9e1d28c2242db4b6760edf3900be202b009b65b9
Drsamraaye <you@example.com>
2025-09-23 14:49:37 +0300

    update component

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -22,21 +22,18 @@ export default function HeroSection() {
     return (
         <>
             <HeroHeader />
-            <main id='Home' className="overflow-x-hidden ">
-         <section className="bg-zinc-300 bg-gradient-to-br from-zinc-300 to-white text-white min-h-screen flex items-center justify-center p-4 **pt-24**">
+            <main id='Home' className="overflow-x-hidden bg-zinc-300">
+         <section className=" bg-gradient-to-br from-zinc-300 to-white text-white p-4 pt-32">
   <div className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-12 px-4 md:px-8">
     {/* Left side: Text and button */}
     <div className="text-center md:text-left md:w-1/2">
       <h1 className="text-4xl sm:text-5xl lg:text-7xl font-extrabold text-black leading-tight tracking-tight">
-      Hi, I&apos;m <br />Full-stack Developer
+        Hi, I&apos;m <br />Full-stack Developer
       </h1>
       <p className="mt-6 text-lg sm:text-xl text-black max-w-lg mx-auto md:mx-0">
         I am a passionate Full-Stack Developer and Digital Marketer, dedicated to building modern, responsive, and user-friendly web applications.
       </p>
-      <a
-        href="#Resume"
-        className="mt-10 inline-block px-8 py-3 text-sm font-semibold bg-black text-white border-2 border-black rounded-full transition-colors duration-300 hover:bg-black hover:text-zinc-600"
-      >
+      <a href="#Resume" className="mt-17 inline-block px-8 py-3 text-sm font-semibold bg-black text-white border-2 border-black rounded-full transition-colors duration-300 hover:bg-black hover:text-zinc-600">
         Resume
       </a>
     </div>
@@ -47,10 +44,10 @@ export default function HeroSection() {
         {/* Inner placeholder to mimic a screen */}
         <div className="absolute inset-2 rounded-full bg-zinc-300 flex items-center justify-center">
           <Image 
-          src="/imges/WhatsApp_Image_2025-09-17_at_18.40.19_8ac072c5-removebg-preview.png"
-          alt=""
-          fill
-          style={{objectFit:"cover"}}
+            src="/imges/WhatsApp_Image_2025-09-17_at_18.40.19_8ac072c5-removebg-preview.png"
+            alt=""
+            fill
+            style={{objectFit:"cover"}}
           />
         </div>
       </div>
