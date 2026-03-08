67426ab573da8f8a79aa96ee462288fdeff919a9
Guiled Hussein <gxuseen79@gmail.com>
2026-03-08 17:41:56 +0300

    l

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -7,7 +7,9 @@ export async function POST(req: Request) {
 
         // 1. Habaynta adeegga Gmail
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.GMAIL_USER,
                 pass: process.env.GMAIL_APP_PASSWORD,
